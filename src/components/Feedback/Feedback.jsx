c9ae8bcb386111d33fb7c20eb20d9f93e7d0d7cc
itishnic <122455719+itishnic@users.noreply.github.com>
2024-06-12 22:31:07 +0300

    commit

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -8,7 +8,7 @@ const Feedback = ({ value }) => {
         <p>Total: {value.good + value.neutral + value.bad}</p>
         <p>
           Positive:
-          {(value.good * 100) / (value.good + value.bad)
+           {(value.good * 100) / (value.good + value.bad)
             ? Math.ceil((value.good * 100) / (value.good + value.bad))
             : ""}
            %
